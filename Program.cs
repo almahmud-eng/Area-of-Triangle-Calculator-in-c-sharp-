9c3c5229d2bfcf706b29af27379f226a34e751cc
almahmud-eng <almahmudaponofficial@gmail.com>
2026-05-03 02:38:47 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Triangle Area = 0.5*base*height
+            double baseValue;
+            double height;
+            Console.WriteLine("Triangle Area Calculator");
+             
+            Console.WriteLine("Input Value of base to calculate the area of triangle = ");
+            baseValue = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Input Value of height to calculate the area of triangle = ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            double AreaOfTriangle = 0.5 * baseValue * height;
+
+            Console.WriteLine("Area of Triangle is = " + AreaOfTriangle);
+
+            Console.ReadLine();
+
+        }
+    }
+}
